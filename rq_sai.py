1cd8043e8f6d1c92ca3e858da8e0c42de9dd9809
Sai Phaltankar <saiphaltankar@gmail.com>
2018-05-12 17:42:03 -0500

    added reult dictionary to be returned for each function

--- rq_sai.py
+++ rq_sai.py
@@ -12,6 +12,7 @@ def getPostCondStats():
     print('Attempting to retrieve jenkinsfile(s) from various GitHub repositories through it\'s REST API...')
     # We are aiming to retrieve ~1000 Jenkinsfiles that are available on GitHub, as every page has max 100 results.
     post_block = defaultdict(int)
+    post_conditions = []
     for page_number in range(1, 10):
         try:
             repositories = jenkinsfile_query('post', page_number)
@@ -24,27 +25,38 @@ def getPostCondStats():
                 file_pointer.close()
                 file_pointer = open('Jenkinsfile.txt', 'r')
                 file_content = file_pointer.readlines()
+                post_cond = {'url': repo['url'], 'post_conditions': []}
                 for line in file_content:
                     if re.search(r'\balways\b\s*\{', line):
                         post_block['always'] += 1
+                        post_cond['post_conditions'].append('always')
                     elif re.search(r'\bsuccess\b\s*\{', line):
                         post_block['success'] += 1
+                        post_cond['post_conditions'].append('success')
                     elif re.search(r'\bfailure\b\s*\{', line):
                         post_block['failure'] += 1
+                        post_cond['post_conditions'].append('failure')
                     elif re.search(r'\bchanged\b\s*\{', line):
                         post_block['changed'] += 1
+                        post_cond['post_conditions'].append('changed')
                     elif re.search(r'\bfixed\b\s*\{', line):
                         post_block['fixed'] += 1
+                        post_cond['post_conditions'].append('fixed')
                     elif re.search(r'\bregression\b\s*\{', line):
                         post_block['regression'] += 1
+                        post_cond['post_conditions'].append('regression')
                     elif re.search(r'\baborted\b\s*\{', line):
                         post_block['aborted'] += 1
+                        post_cond['post_conditions'].append('aborted')
                     elif re.search(r'\bunstable\b\s*\{', line):
                         post_block['unstable'] += 1
+                        post_cond['post_conditions'].append('unstable')
                     elif re.search(r'\bcleanup\b\s*\{', line):
                         post_block['cleanup'] += 1
+                        post_cond['post_conditions'].append('cleanup')
 
                 file_pointer.close()
+                post_conditions.append(post_cond)
 
         except Exception as e:
             print(e)
@@ -55,10 +67,30 @@ def getPostCondStats():
     min_post_block = min(post_block.keys(), key=(lambda k: post_block[k]))
     print("The most frequent post condition is " + max_post_block + " and least frequent is " + min_post_block)
 
+    min_value = min(post_block.values())
+    max_value = max(post_block.values())
+    min_result = [(key, value) for key, value in post_block.iteritems() if value == min_value]
+    max_result = [(key, value) for key, value in post_block.iteritems() if value == max_value]
+
+    max_dict_list = []
+    for key in max_result:
+        max_dict_list.append({'post_condition': key, 'count': max_result[key]})
+
+    min_dict_list = []
+    for key in min_result:
+        min_dict_list.append({'post_condition': key, 'count': min_result[key]})
+
+    result = {'most_frequent': max_dict_list,
+              'least_frequent': min_dict_list,
+              'post_conditions': post_conditions}
+
+    return result
+
 
 def getPipelineStageStats():
     stages = defaultdict(int)
     stageCountAll = list()
+    stagesPerFile = list()
     for page_number in range(1, 10):
         try:
             repositories = jenkinsfile_query('stages', page_number)
@@ -72,46 +104,52 @@ def getPipelineStageStats():
                 file_pointer = open('Jenkinsfile.txt', 'r')
                 file_content = file_pointer.readlines()
                 stagecount = 0
-                triggercount = 0
-                insidetrigger = False
-                trigger = ''
+                stagesDict = {'url': repo['url'], 'stages': []}
                 for line in file_content:
                     m = re.search(r"""\bstage\b\s*\((["'])(?:(?=(\\?))\2.)*?\1\)""", line)
                     if m:
                         stagestr = m.group(0).replace("stage('", "").replace("')", '')
                         stages[stagestr] += 1
                         stagecount += 1
-                    else:
-                        t = re.search(r"triggers\s+\{", line)
-                        #t = re.search(r"^(cron|pollSCM|upstream)", line)
-                        if t:
-                            insidetrigger = True
-                        if insidetrigger:
-                            print(line)
-                            trigger.join(line)
-                            if re.search(r"\s+\}", line):
-                                insidetrigger = False
-                            elif re.search(r"(cron|pollSCM|upstream)", line):
-                                triggercount += 1
-
+                        stagesDict['stages'].append(stagestr)
                 file_pointer.close()
-                print(trigger)
                 print(stagecount)
-                print(triggercount)
-                stageCountAll.append((stagecount, triggercount))
 
+                stageCountAll.append(stagecount)
+                stagesPerFile.append(stagesDict)
         except Exception as e:
             print(e)
 
-    #print(stages)
     max_stages = max(stages.keys(), key=(lambda k: stages[k]))
     min_stages = min(stages.keys(), key=(lambda k: stages[k]))
     print("The most popular stage is " + max_stages + " and least popular stage is " + min_stages)
+
+    min_value = min(stages.values())
+    max_value = max(stages.values())
+    min_result = [(key, value) for key, value in stages.iteritems() if value == min_value]
+    max_result = [(key, value) for key, value in stages.iteritems() if value == max_value]
+
+    max_stages_dict_list = []
+    for key in max_result:
+        max_stages_dict_list.append({'stage': key, 'count': max_result[key]})
+
+    min_stages_dict_list = []
+    for key in min_result:
+        min_stages_dict_list.append({'stage': key, 'count': min_result[key]})
+
+    mean_stages = float(sum(stageCountAll)) / max(len(stageCountAll), 1)
+    result = {'most_popular_operation': max_stages_dict_list,
+              'least_popular_operation': min_stages_dict_list,
+              'avg_stages': mean_stages,
+              'stages_per_file': stagesPerFile}
+
+    return result
 
 
 def getTriggerStagesCorelation():
     counts = {'stages': 0, 'triggers': 0, 'stagetrigger': 0, 'stagesq': 0, 'triggersq': 0}
     totalfiles = 0
+    jenkinsfiles = []
     for page_number in range(1, 2):
         try:
             repositories = jenkinsfile_query('triggers+stages', page_number)
@@ -153,6 +191,7 @@ def getTriggerStagesCorelation():
                     counts['stagetrigger'] += (stagecount*triggercount)
                     counts['stagesq'] += (stagecount**2)
                     counts['triggersq'] += (triggercount**2)
+                    jenkinsfiles.append({'url': repo['url'], 'stages': stagecount, 'triggers': triggercount})
                 file_pointer.close()
         except Exception as e:
             print(e)
@@ -161,3 +200,8 @@ def getTriggerStagesCorelation():
     d = math.sqrt((totalfiles * counts['stagesq'] - counts['stages']**2) * (totalfiles * counts['triggersq'] - counts['triggers']**2))
     corr_coeff = n / d
     print('coeff is %.3f', corr_coeff)
+    result = {'correlation_coefficient': corr_coeff,
+              'total_stages': counts['stages'],
+              'total_triggers': counts['triggers'],
+              'jenkinsfiles_analysed': jenkinsfiles}
+    return result
