2122ca9597cda5c1e5079fa008c22c963244b68e
Pratik Kshirsagar <pratikkshirsagar@Pratiks-MBP.hsd1.il.comcast.net>
2018-05-12 03:45:13 -0500

    Added docker analysis.

--- P.py
+++ P.py
@@ -83,20 +83,16 @@ def readyFile(jenkinsfile_content): # to create the jenkinsfile parseable in txt
 # # eh = exception_handling()
 # # print(eh)
 #
-# def docker_agent():
-
-def popular_agent(): # Find the most popular kind of agent in jenkinsfiles.
-
-    none_count = 0
-    node_count = 0
-    any_count = 0
-    label_count = 0
-    docker_count = 0
-    dockerfile_count = 0
+def docker():
     number = 1
+    count_docker = 0
+    count_dockerfile = 0
+
+    # \bdocker\b\s*\{
+    # \bdockerfile\b\s*\{
 
     for page_number in range(1,10): # We are aiming to retrieve ~1000 Jenkinsfiles that are available on GitHub, as every page has max 100 results.
-        repositories = jenkinsfile_query('agent', page_number)
+        repositories = jenkinsfile_query('docker', page_number)
         try:
 
             for repo in repositories.json()['items']:
@@ -106,45 +102,90 @@ def popular_agent(): # Find the most popular kind of agent in jenkinsfiles.
                 file_content = readyFile(jenkinsfile_content)
 
                 for line in file_content:
+                    if re.search(r'\bdocker\b\s*\{', line): # using regex to find the occurence of keyword 'docker'.
+                        count_docker = count_docker + 1 # increment count of total jenkinsfiles that use 'docker' in some way.
 
-                    if re.search(r'\bagent\b\s*\bnone\b', line): # using regex to find the occurence of an agent of type 'none'.
-                        none_count = none_count + 1 # increment count of total jenkinsfiles that have an agent of type 'none'.
-
-                    if re.search(r'\bagent\b\s*\bany\b', line): # using regex to find the occurence of an agent of type 'any'.
-                        any_count = any_count + 1 # increment count of total jenkinsfiles that have an agent of type 'any'.
-
-                    if re.search(r'\bagent\b\s*\{\s*\blabel\b', line): # using regex to find the occurence of an agent of type 'label'.
-                        label_count = label_count + 1 # increment count of total jenkinsfiles that have an agent of type 'label'.
-
-                    if re.search(r'\bagent\b\s*\{\s*\bnode\b', line): # using regex to find the occurence of an agent of type 'node'.
-                        node_count = node_count + 1 # increment count of total jenkinsfiles that have an agent of type 'node'.
-
-                    if re.search(r'\bagent\b\s*\{\s*\bdocker\b', line): # using regex to find the occurence of an agent of type 'docker'.
-                        docker_count = docker_count + 1 # increment count of total jenkinsfiles that have an agent of type 'docker'.
-
-                    if re.search(r'\bagent\b\s*\{\s*\bdockerfile\b', line): # using regex to find the occurence of an agent of type 'dockerfile'.
-                        dockerfile_count = dockerfile_count + 1 # increment count of total jenkinsfiles that have an agent of type 'dockerfile'.
-
+                    if re.search(r'\bdockerfile\b\s*\{', line): # using regex to find the occurence of keyword 'dockerfile'.
+                        count_dockerfile = count_dockerfile + 1 # increment count of total jenkinsfiles that use 'dockerfile' in any way.
 
                 number = number + 1
 
         except Exception as e:
-            print('Error occurred in finding out which kinds of agent are the most popular.')
-            logging.error('Error occurred while finding out the most popular kind of agent.')
+            print('Error occurred in finding out what % of jenkins pipelines use docker.')
+            logging.error('Error occurred while finding out what % of jenkins pipelines use docker.')
 
     ''' Generate percentage of occurrences, actually a fraction'''
-    percent_any = any_count / (number - 1)
-    percent_node = node_count / (number - 1)
-    percent_none = none_count / (number - 1)
-    percent_label = label_count / (number - 1)
-    percent_docker = docker_count / (number - 1)
-    percent_dockerfile = dockerfile_count / (number - 1)
+    percent_docker = count_docker / (number - 1)
+    percent_dockerfile = count_dockerfile / (number - 1)
+    percent_total = percent_docker + percent_dockerfile
 
-    agent_dictionary = {'any' : percent_any, 'node' : percent_node, 'none' : percent_none, 'label' : percent_label, 'docker' : percent_docker, 'dockerfile' : percent_dockerfile}
-    return agent_dictionary
+    docker_dictionary = {'docker' : percent_docker, 'dockerfile' : percent_dockerfile, 'total' : percent_total}
+    return docker_dictionary
 
-agent = popular_agent()
-print(agent)
+docker_stats = docker()
+print(docker_stats)
+
+
+# def popular_agent(): # Find the most popular kind of agent in jenkinsfiles.
+#
+#     none_count = 0
+#     node_count = 0
+#     any_count = 0
+#     label_count = 0
+#     docker_count = 0
+#     dockerfile_count = 0
+#     number = 1
+#
+#     for page_number in range(1,10): # We are aiming to retrieve ~1000 Jenkinsfiles that are available on GitHub, as every page has max 100 results.
+#         repositories = jenkinsfile_query('agent', page_number)
+#         try:
+#
+#             for repo in repositories.json()['items']:
+#                 print(repo['url'])
+#                 logging.info(repo['url'])
+#                 jenkinsfile_content = contents_query(repo['url'])
+#                 file_content = readyFile(jenkinsfile_content)
+#
+#                 for line in file_content:
+#
+#                     if re.search(r'\bagent\b\s*\bnone\b', line): # using regex to find the occurence of an agent of type 'none'.
+#                         none_count = none_count + 1 # increment count of total jenkinsfiles that have an agent of type 'none'.
+#
+#                     if re.search(r'\bagent\b\s*\bany\b', line): # using regex to find the occurence of an agent of type 'any'.
+#                         any_count = any_count + 1 # increment count of total jenkinsfiles that have an agent of type 'any'.
+#
+#                     if re.search(r'\bagent\b\s*\{\s*\blabel\b', line): # using regex to find the occurence of an agent of type 'label'.
+#                         label_count = label_count + 1 # increment count of total jenkinsfiles that have an agent of type 'label'.
+#
+#                     if re.search(r'\bagent\b\s*\{\s*\bnode\b', line): # using regex to find the occurence of an agent of type 'node'.
+#                         node_count = node_count + 1 # increment count of total jenkinsfiles that have an agent of type 'node'.
+#
+#                     if re.search(r'\bagent\b\s*\{\s*\bdocker\b', line): # using regex to find the occurence of an agent of type 'docker'.
+#                         docker_count = docker_count + 1 # increment count of total jenkinsfiles that have an agent of type 'docker'.
+#
+#                     if re.search(r'\bagent\b\s*\{\s*\bdockerfile\b', line): # using regex to find the occurence of an agent of type 'dockerfile'.
+#                         dockerfile_count = dockerfile_count + 1 # increment count of total jenkinsfiles that have an agent of type 'dockerfile'.
+#
+#
+#                 number = number + 1
+#
+#         except Exception as e:
+#             print('Error occurred in finding out which kinds of agent are the most popular.')
+#             logging.error('Error occurred while finding out the most popular kind of agent.')
+#
+#     ''' Generate percentage of occurrences, actually a fraction'''
+#     percent_any = any_count / (number - 1)
+#     percent_node = node_count / (number - 1)
+#     percent_none = none_count / (number - 1)
+#     percent_label = label_count / (number - 1)
+#     percent_docker = docker_count / (number - 1)
+#     percent_dockerfile = dockerfile_count / (number - 1)
+#
+#     agent_dictionary = {'any' : percent_any, 'node' : percent_node, 'none' : percent_none, 'label' : percent_label, 'docker' : percent_docker, 'dockerfile' : percent_dockerfile}
+#     return agent_dictionary
+#
+# agent = popular_agent()
+# print(agent)
 
 # def global_agent():
 
