1caaf14d48762a2b56f2572eafbe97b1ddb067be
Pratik Kshirsagar <pratikkshirsagar@10-107-227-240.vlan910.client.wireless.uic.edu>
2018-05-12 16:34:44 -0500

    Unittests

---
+++ unittests.py
@@ -0,0 +1,34 @@
+from P import jenkinsfile_query
+from rq_sai import *
+import unittest
+import os
+import requests
+
+
+    # def test_jenkinsfile_query_ex_handle(self):
+    #     self.assertIsNotNone(jenkinsfile_query('try', 1))
+    #
+    # def test_jenkinsfile_query_agent(self):
+    #     self.assertIsNotNone(jenkinsfile_query('agent', 1))
+    #
+    # def test_jenkinsfile_query_docker(self):
+    #     self.assertIsNotNone(jenkinsfile_query('docker', 1))
+    #
+    # def test_jenkinsfile_query_post(self):
+    #     self.assertIsNotNone(jenkinsfile_query('post', 1))
+    #
+    # def test_jenkinsfile_query_stages(self):
+    #     self.assertIsNotNone(jenkinsfile_query('stages', 1))
+
+def test_regex(self):
+    self.assertIsNotNone(re.search(r'\bagent\b\s*\bnone\b', 'agent none'))
+    self.assertIsNotNone(re.search(r'\btry\b\s*\{', 'try {'))
+    self.assertIsNotNone(re.search(r'\bdocker\b\s*\{', 'docker {'))
+
+
+# Test the http requests
+# Test the functions
+
+test_regex()
+
+
